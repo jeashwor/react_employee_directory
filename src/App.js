b8b2f3575d48932f45929995c30c6dffe44b8d2c
Jacob Ashworth <jeashwor@gmail.com>
2020-10-23 10:30:44 -0400

    added wrapper component and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 import EmployeeDirectory from "./pages/EmployeeDirectory";
 
 
