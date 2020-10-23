5e973d3309048680c1cad5ec947b5c6b43e82f68
Jacob Ashworth <jeashwor@gmail.com>
2020-10-23 18:21:15 -0400

    completed card element and populated sort function into Employee Directory

--- src/components/Card/indes.js
+++ src/components/Card/indes.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
+    var DOB = new Date(props.date);
+
     return (
         <div className="card">
             <div><img alt={props.last} src={props.image}></img></div>
             <div>{props.title} {props.first} {props.last} </div>
-            <div>{props.age}</div>
+            <div>{DOB}</div>
             <div>{props.phone}</div>
+            <div>{props.cell}</div>
             <div>{props.email}</div>
         </div>
     );
--- src/pages/EmployeeDirectory.js
+++ src/pages/EmployeeDirectory.js
@@ -1,7 +1,83 @@
 import React, { Component } from "react";
+import Card from "../components/Card/indes";
+import search from "../utils/API";
+
 
 class EmployeeDirectory extends Component {
+    state = {
+        sortedEmployees: [],
+        employees: [],
+        sorted: true
+    };
 
+    componentDidMount() {
+        search().then(res => {
+            this.setState({ employees: res.data.results });
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.empList !== prevProps.empList) {
+            this.setState({
+                sortedEmployees: this.props.empList
+            })
+        }
+    }
+
+    sortItem = () => {
+        let sortList = [];
+        if (this.state.sorted) {
+            sortList = this.props.empList.sort((a, b) => {
+                let itemA = a.name.last.toLowerCase();
+                let itemB = b.name.last.toLowerCAse();
+                if (itemA < itemB)
+                    return -1
+                if (itemA > itemB)
+                    return 1
+                return 0
+            })
+        } else {
+            sortList = this.props.empList.sort((a, b) => {
+                let itemA = a.name.last.toLowerCase();
+                let itemB = b.name.last.toLowerCAse();
+                if (itemA > itemB)
+                    return -1
+                if (itemA < itemB)
+                    return 1
+                return 0
+            })
+        }
+        this.setState({
+            sorted: !this.state.sorted,
+            sortedEmployees: sortList
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    <div>Photo</div>
+                    <div><p onClick={this.sortItem} className="name">Name</p></div>
+                    <div>DOB</div>
+                    <div>Phone</div>
+                    <div>Email</div>
+                </div>
+                { this.state.sortedEmployees.length > 0 && this.state.sortedEmployees.map((item, index) => (
+                    <Card
+                        image={item.picture.medium}
+                        title={item.name.title}
+                        first={item.name.first}
+                        last={item.name.last}
+                        age={item.dob.date}
+                        phone={item.phone}
+                        cell={item.cell}
+                        email={item.email}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default EmployeeDirectory;
