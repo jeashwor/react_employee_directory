b08d797addb1f8ad6d6d34d89f2b35b4121366c3
Jacob Ashworth <jeashwor@gmail.com>
2020-10-23 10:18:00 -0400

    added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header () {
+    return (
+        <header className="header">
+            <h1>React Employee Directory</h1>
+            <p> Write Instructions here!!!!!</p>
+        </header>
+    );
+}
+
+export default Header;
