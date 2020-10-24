2f0d364ac046cb4a0194cf66e36caf39080c6364
Jacob Ashworth <jeashwor@gmail.com>
2020-10-23 23:44:48 -0400

    Updates

---
+++ precache-manifest.a25885c1f32285147acdceb9007c2c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524a3da6d7c4fd0b3987355da5efdcce",
+    "url": "/react_employee_directory/index.html"
+  },
+  {
+    "revision": "b9a9e84f05ba53e596f8",
+    "url": "/react_employee_directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8d6d45ab8cd6450f1c85",
+    "url": "/react_employee_directory/static/css/main.8be8dc70.chunk.css"
+  },
+  {
+    "revision": "b9a9e84f05ba53e596f8",
+    "url": "/react_employee_directory/static/js/2.49e7716a.chunk.js"
+  },
+  {
+    "revision": "857af1c517e1f86bdb841a8a5ccc520a",
+    "url": "/react_employee_directory/static/js/2.49e7716a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6d45ab8cd6450f1c85",
+    "url": "/react_employee_directory/static/js/main.2832d7ae.chunk.js"
+  },
+  {
+    "revision": "dd19cdf92f7b340a35f3",
+    "url": "/react_employee_directory/static/js/runtime-main.12aab73b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f405da3c1d818d3482d04e689ad9299f.js"
+  "/react_employee_directory/precache-manifest.a25885c1f32285147acdceb9007c2c1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_employee_directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
