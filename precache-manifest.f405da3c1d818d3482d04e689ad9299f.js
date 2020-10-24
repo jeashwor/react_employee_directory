bde4ab6cefaaaff74496fbc9998cd8c3a7ea00a2
Jacob Ashworth <jeashwor@gmail.com>
2020-10-23 23:40:26 -0400

    Updates

---
+++ precache-manifest.f405da3c1d818d3482d04e689ad9299f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcb7b74833a5c22b2df13c2848771a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9a9e84f05ba53e596f8",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "faa154e65be9cda0a5c9",
+    "url": "/static/css/main.8be8dc70.chunk.css"
+  },
+  {
+    "revision": "b9a9e84f05ba53e596f8",
+    "url": "/static/js/2.49e7716a.chunk.js"
+  },
+  {
+    "revision": "857af1c517e1f86bdb841a8a5ccc520a",
+    "url": "/static/js/2.49e7716a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa154e65be9cda0a5c9",
+    "url": "/static/js/main.603eee69.chunk.js"
+  },
+  {
+    "revision": "93dbc0720728f92ecbc4",
+    "url": "/static/js/runtime-main.78bf1add.js"
+  }
+]);
